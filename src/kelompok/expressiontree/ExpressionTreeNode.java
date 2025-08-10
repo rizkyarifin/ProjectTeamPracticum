05ed65e67fa3dd7e172e14c51297433bbe151c41
Vincent <128157951+VcnWuisan@users.noreply.github.com>
2025-08-10 21:59:09 +0700

    Update ExpressionTreeNode.java

--- src/kelompok/expressiontree/ExpressionTreeNode.java
+++ src/kelompok/expressiontree/ExpressionTreeNode.java
@@ -1,17 +1,37 @@
 package kelompok.expressiontree;
 
 /**
- * Node untuk Expression Tree.
- * Menyimpan data (operator/operand) dan referensi ke anak kiri dan kanan.
+ * Kelas Node untuk Expression Tree.
+ * Menyimpan nilai (angka/operator) dan referensi ke anak kiri & kanan.
  */
 public class ExpressionTreeNode {
-    public String value;
-    public ExpressionTreeNode left, right;
+    private String value;
+    private ExpressionTreeNode left;
+    private ExpressionTreeNode right;
 
     public ExpressionTreeNode(String value) {
         this.value = value;
         this.left = null;
         this.right = null;
     }
-}
 
+    public String getValue() {
+        return value;
+    }
+
+    public ExpressionTreeNode getLeft() {
+        return left;
+    }
+
+    public ExpressionTreeNode getRight() {
+        return right;
+    }
+
+    public void setLeft(ExpressionTreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(ExpressionTreeNode right) {
+        this.right = right;
+    }
+}
